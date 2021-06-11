d3f89b726fb4a3956172b1bb90e59e6f51c35c84
ungober <ungober@gmail.com>
2021-06-11 10:42:45 -0500

    add general

---
+++ vikiland/js/main.js
@@ -0,0 +1,7 @@
+
+var hamburguer = document.getElementById('Hamburguer')
+var menu = document.getElementById('Menu')
+
+hamburguer.addEventListener("click", function(){
+    menu.classList.toggle('open')
+})
