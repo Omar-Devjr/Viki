5c78381c136ef480a441b2de5a8385401761eddc
Proxy117 <56364490+Omar-Devjr@users.noreply.github.com>
2021-06-18 15:03:25 -0500

    Delete vikiland directory

--- vikiland/js/main.js
+++
@@ -1,7 +0,0 @@
-
-var hamburguer = document.getElementById('Hamburguer')
-var menu = document.getElementById('Menu')
-
-hamburguer.addEventListener("click", function(){
-    menu.classList.toggle('open')
-})
